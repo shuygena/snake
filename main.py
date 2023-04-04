29688b3b7d6ff6611315aa32aff2161ea7a322d5
shuygena <i.am.zuzya@gmail.com>
2023-04-04 08:26:49 +0300

    add moving

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import os
-from pygame import display, event, QUIT, quit, draw
+from sys import exit
+from pygame import display, event, QUIT, quit, draw, time, KEYDOWN, K_DOWN, K_UP, K_LEFT, K_RIGHT
 from snake.snake import SnakeBlock
 # os.environ["SDL_VIDEODRIVER"] = "dummy" for Linux
 # os.environ["SDL_VIDEODRIVER"]="x11" # for WSL
@@ -11,6 +12,7 @@ FRAME_COLOR = (223, 223, 223)
 # HEADER_COLOR = (171, 171, 212)
 HEADER_COLOR = (190, 190, 212)
 SNAKE_COLOR = (5, 115, 82)
+APPLE_COLOR = (180, 1, 1)
 COUNT_BLOCKS = 20
 MARGIN = 1
 HEAD_MARGIN = 70
@@ -25,10 +27,20 @@ def draw_block(screen, color, row, column):
 
 
 def draw_snake(screen, snake):
-    draw_block(screen, SNAKE_COLOR, snake.x, snake.y)
+    x = snake.get_x()
+    y = snake.get_y()
+    draw_block(screen, SNAKE_COLOR, x, y)
 
 
-def draw_field(screen):
+def move_snake(screen, snake_blocks, d_row, d_col):
+    head = snake_blocks[-1]
+    new_head = SnakeBlock(head.get_x() + d_row, head.get_y() + d_col)
+    snake_blocks.append(new_head)
+    snake_blocks.pop(0)
+
+
+def draw_field(screen, snake_blocks, d_row, d_col):
+    draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEAD_MARGIN])
     for row in range(COUNT_BLOCKS):
         for column in range(COUNT_BLOCKS):
             if (column + row) % 2 == 0:
@@ -36,22 +48,47 @@ def draw_field(screen):
             else:
                 color = GHOSTLY_WHITE
             draw_block(screen, color, row, column)
-            snake = SnakeBlock(9, 9)
-            draw_snake(screen, snake)
+    if not snake_blocks[-1].is_inside(SIZE_BLOCK):
+        quit()
+        exit()
+    for block in snake_blocks:
+        draw_snake(screen, block)
+    move_snake(screen, snake_blocks, d_row, d_col)
 
 
 def game_loop():
+    timer = time.Clock()
     screen = display.set_mode(size)
     screen.fill(FRAME_COLOR)
-    draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEAD_MARGIN])
+    display.set_caption("Змейка")
+    d_row = 0
+    d_col = 1
+    snake_blocks = [SnakeBlock(9, 8), SnakeBlock(9, 9), SnakeBlock(9, 10)]
     while True:
+        # d_col = 0
+        # d_row = 0
         for ev in event.get():
             if ev.type == QUIT:
                 quit()
-        display.set_caption("Змейка")
-        draw_field(screen)
+                exit()
+            elif ev.type == KEYDOWN:
+                if ev.key == K_UP and d_col != 0:
+                    d_row = -1
+                    d_col = 0
+                elif ev.key == K_DOWN and d_col != 0:
+                    d_row = 1
+                    d_col = 0
+                elif ev.key == K_RIGHT and d_row != 0:
+                    d_row = 0
+                    d_col = 1
+                elif ev.key == K_LEFT and d_row != 0:
+                    d_row = 0
+                    d_col = -1
+        draw_field(screen, snake_blocks, d_row, d_col)
         display.flip()
-
+        timer.tick(2)
+        # timer = time.Clock()
+        
 
 if __name__ == '__main__':
     game_loop()
--- snake/snake.py
+++ snake/snake.py
@@ -1,4 +1,19 @@
 class SnakeBlock:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    def get_x(self):
+        return self._x
+    
+    def get_y(self):
+        return self._y
+    
+    def move_x(self, x):
+        self._x += x
+    
+    def move_y(self, y):
+        self._y += y
+
+    def is_inside(self, size_block):
+        return 0 <= self._x < size_block and 0 <= self._y < size_block
